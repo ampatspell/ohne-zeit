9702e2d6914f828fc9a5188897f56f5e38ca3f5e
ampatspell <ampatspell@gmail.com>
2018-04-14 19:59:36 +0300

    move store to zuglet

--- app/instance-initializers/ohne-zeit-injections.js
+++ app/instance-initializers/ohne-zeit-injections.js
@@ -1,6 +1,5 @@
 export default {
   name: 'ohne-zeit:injections',
-  after: 'ohne-zeit:store',
   initialize(app) {
     app.inject('component', 'router', 'service:router');
   }
--- app/instance-initializers/ohne-zeit-store.js
+++ app/instance-initializers/ohne-zeit-store.js
@@ -1,38 +1,24 @@
-import registerContextService from 'ember-cli-zug/register-context-service';
-import registerModelService from 'ember-cli-zug/register-model-service';
-
-let firebase = {
-  apiKey: "AIzaSyDyjC_rsH7_BYJwjKgIrHhoSvRBfNnjGrQ",
-  authDomain: "ohne-zeit.firebaseapp.com",
-  databaseURL: "https://ohne-zeit.firebaseio.com",
-  projectId: "ohne-zeit",
-  storageBucket: "ohne-zeit.appspot.com",
-  messagingSenderId: "491555737764"
-};
-
-let modelNameForDocument = document => {
-  if(document.get('collection') === 'messages') {
-    return 'message';
-  }
-};
+import register from 'ember-cli-zuglet/register';
+import Store from '../store';
 
 export default {
   name: 'ohne-zeit:store',
   initialize(app) {
-
-    // register root context as a 'store' service
-    let store = registerContextService({
+    register({
       app,
-      firebase,
-      modelNameForDocument
+      store: {
+        identifier: 'store',
+        factory: Store
+      },
+      // service: {
+      //   enabled: true,
+      //   name: 'store',
+      //   inject: [ 'route', 'controller', 'component' ],
+      // },
+      // development: {
+      //   enabled: true,
+      //   export: 'store'
+      // }
     });
-
-    // create transient model in root context and register it as a `state` service
-    registerModelService({
-      app,
-      store,
-      name: 'state'
-    });
-
   }
 };
