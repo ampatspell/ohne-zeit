e48fe162226b7258f91e1194033d0d9406769dc5
Arnis Vuskans <ampatspell@gmail.com>
2019-06-07 02:44:31 +0300

    remove ember-animated, add asset cache

--- app/router.js
+++ app/router.js
@@ -1,9 +1,29 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
+let _doneLoading = false;
+const doneLoading = () => {
+  if(_doneLoading) {
+    return;
+  }
+  _doneLoading = true;
+  let el = document.querySelector('.ui-application-loading');
+  el.style.display = 'none';
+}
+
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  routeDidChange() {
+    doneLoading();
+  },
+
+  init() {
+    this._super(...arguments);
+    this.on('routeDidChange', () => this.routeDidChange());
+  }
+
 });
 
 Router.map(function() {
